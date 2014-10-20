10ceb17b760fcc67f19e4b873b6ab41713294fe3
Tim Wade <tim@majority.co>
2014-10-20 09:29:32 -0700

    Tidy.

--- run.rb
+++ run.rb
@@ -7,12 +7,13 @@ def strip_tags(string)
 end
 
 def stitch_paragraphs(paragraphs)
-  paragraphs.map { |p| p.to_s }.join("\n\n")
+  paragraphs.join("\n\n")
 end
 
 uri = open('https://en.wikipedia.org/wiki/Alfred_Hitchcock')
 page = Nokogiri::HTML(uri)
-paragraphs = page.css('p')
+paragraphs = page.css('p').map { |paragraph| paragraph.to_s }
+
 html = stitch_paragraphs(paragraphs)
 text = strip_tags(html)
 
